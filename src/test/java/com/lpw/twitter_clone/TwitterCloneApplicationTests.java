667c112e90eabb4b7ce69ee22186ce5b0b023d10
Lucas <lucas-weible@defensoria.rs.def.br>
2022-09-12 07:53:40 -0300

    first

---
+++ src/test/java/com/lpw/twitter_clone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpw.twitter_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
