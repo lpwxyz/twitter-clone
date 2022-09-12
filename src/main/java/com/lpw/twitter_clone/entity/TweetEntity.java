b2271f6741b79b0f0130dfca67883501fdabbb39
Lucas <lucas-weible@defensoria.rs.def.br>
2022-09-12 11:54:14 -0300

    add some stuff

---
+++ src/main/java/com/lpw/twitter_clone/entity/TweetEntity.java
@@ -0,0 +1,30 @@
+package com.lpw.twitter_clone.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "tweets")
+public class TweetEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+}
---
+++ src/main/java/com/lpw/twitter_clone/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package com.lpw.twitter_clone.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "dob", nullable = false)
+    private LocalDate dob;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+//    @Column(name = "tweets")
+    @OneToMany(mappedBy = "user")
+    private List<TweetEntity> tweets;
+}
